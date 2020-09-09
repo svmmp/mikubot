e17d46b62d1009c761ad4cdb7131905c2f84c00e
unknown <sammypavlish@gmail.com>
2020-09-08 20:33:30 -0400

    Adding simple bot code!

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace mikubot
 {
     class Program
     {
+        private readonly DiscordSocketClient _client;
         private readonly IConfiguration _config;
-        private DiscordSocketClient _client;
 
         static void Main(string[] args)
         {
@@ -21,37 +21,32 @@ namespace mikubot
 
         public Program()
         {
+            _client = new DiscordSocketClient();
+
+            //Hook into log event and write it out to the console
+            _client.Log += LogAsync;
+
+            //Hook into the client ready event
+            _client.Ready += ReadyAsync;
+
+            //Hook into the message received event, this is how we handle the hello world example
+            _client.MessageReceived += MessageReceivedAsync;
+
+            //Create the configuration
             var _builder = new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile(path: "config.json");  
-        
+                .AddJsonFile(path: "config.json");            
             _config = _builder.Build();
         }
 
         public async Task MainAsync()
         {
-            // call ConfigureServices to create the ServiceCollection/Provider for passing around the services
-            using (var services = ConfigureServices())
-            {
-                // get the client and assign to client 
-                // you get the services via GetRequiredService<T>
-                var client = services.GetRequiredService<DiscordSocketClient>();
-                _client = client;
+            //This is where we get the Token value from the configuration file
+            await _client.LoginAsync(TokenType.Bot, _config["Token"]);
+            await _client.StartAsync();
 
-                // setup logging and the ready event
-                client.Log += LogAsync;
-                client.Ready += ReadyAsync;
-                services.GetRequiredService<CommandService>().Log += LogAsync;
-
-                // this is where we get the Token value from the configuration file, and start the bot
-                await client.LoginAsync(TokenType.Bot, _config["Token"]);
-                await client.StartAsync();
-
-                // we get the CommandHandler class here and call the InitializeAsync method to start things up for the CommandHandler service
-                await services.GetRequiredService<CommandHandler>().InitializeAsync();
-
-                await Task.Delay(-1);
-            }
+            // Block the program until it is closed.
+            await Task.Delay(-1);
         }
 
         private Task LogAsync(LogMessage log)
@@ -66,19 +61,17 @@ namespace mikubot
             return Task.CompletedTask;
         }
 
-        // this method handles the ServiceCollection creation/configuration, and builds out the service provider we can call on later
-        private ServiceProvider ConfigureServices()
+        //I wonder if there's a better way to handle commands (spoiler: there is :))
+        private async Task MessageReceivedAsync(SocketMessage message)
         {
-            // this returns a ServiceProvider that is used later to call for those services
-            // we can add types we have access to here, hence adding the new using statement:
-            // using csharpi.Services;
-            // the config we build is also added, which comes in handy for setting the command prefix!
-            return new ServiceCollection()
-                .AddSingleton(_config)
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton<CommandService>()
-                .AddSingleton<CommandHandler>()
-                .BuildServiceProvider();
+            //This ensures we don't loop things by responding to ourselves (as the bot)
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "^hello")
+            {
+                await message.Channel.SendMessageAsync("world!");
+            }  
         }
     }
 }
